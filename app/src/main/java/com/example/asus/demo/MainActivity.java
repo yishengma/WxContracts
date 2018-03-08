523e3949446eb9d89823d6df763e84796b9f71e6
mayisheng <13104862372@163.com>
2018-03-08 22:43:55 +0800

    更改图片

--- app/src/main/java/com/example/asus/demo/MainActivity.java
+++ app/src/main/java/com/example/asus/demo/MainActivity.java
@@ -3,33 +3,33 @@ package com.example.asus.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import com.example.asus.demo.adapter.ListTextAdapter;
-import com.example.asus.demo.entity.ListDataEntity;
+import com.example.asus.demo.adapter.ContractsAdapter;
+import com.example.asus.demo.entity.ContractorEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
-    private ListView mListView;
-    private String[] data = {"a","b","c","d","e","f","g","h","i","j","k","l"};
-    private List<ListDataEntity> mList;
+    private List<ContractorEntity> mContractorEntities;
+    private RecyclerView mRvContracts;
+    private ContractsAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mListView = findViewById(R.id.lv);
-        mList = new ArrayList<>();
-        for(int i=0;i<20;i++){
-            ListDataEntity entity = new ListDataEntity();
-            entity.setId(android.R.drawable.btn_star);
-            entity.setString("字母 ："+i);
-            mList.add(entity);
 
-        }
-         ListTextAdapter adapter = new ListTextAdapter(this,R.layout.item_listview,mList);
-        mListView.setAdapter(adapter);
     }
+
+    public void initView(){
+        mRvContracts = (RecyclerView)findViewById(R.id.rv_contracts);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRvContracts.setLayoutManager(manager);
+        mAdapter = new ContractsAdapter(mContractorEntities);
+    }
+
+
+
 }
